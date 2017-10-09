38f3348b9649ef79426c83b09bb890c3b1adef5b
Qsior <lukaszbojes@gmail.com>
2017-10-09 20:56:46 +0200

    String examples

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,25 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String myString = "This is a String";
+        System.out.println(myString);
+        myString = myString + ", and this is more String";
+        System.out.println(myString);
+        myString = myString + " \u00A9 2017";
+        System.out.println(myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("Result = " + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println(lastString);
+
+        double doubleNumer = 120.47;
+        lastString = lastString + doubleNumer;
+        System.out.println(lastString);
+
     }
 }
